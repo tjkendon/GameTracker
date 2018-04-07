3574ce7d1a62a6ee996705cbcffc07cfc9e53495
Tyson Kendon <tj@kendon.ca>
2018-04-06 19:54:33 -0600

    Added name to game and platform Enum to handle gaming platform Information

--- src/gametracker/Game.java
+++ src/gametracker/Game.java
@@ -3,8 +3,39 @@ package gametracker;
 
 /**
  *
+ * Basic data about a game
+ * 
  * @author tjkendon
  */
 public class Game {
+    
+    public enum Platform {
+        PC_Steam,
+        PC_Other,
+        Wii_U,
+        Wii,
+        GameCube,
+        Nintendo_64,
+        Super_Nintendo,
+        DS_3DS,
+        DS,
+        GameBoy_Advance,        
+        Playstation_3,
+        Playstation_2,
+        Playstation,
+        
+    }
+    
+    private final String name;
+
+    public Game(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+    
+    
     
 }
