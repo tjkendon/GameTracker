5495af2edb7ffc9184493b44bc86a7ddbf372c8f
Tyson Kendon <tj@kendon.ca>
2018-04-09 23:34:45 -0600

    Added basic structure to data classes to record a simple set of play sessions

--- src/gametracker/PlaySession.java
+++ src/gametracker/PlaySession.java
@@ -1,4 +1,3 @@
-
 package gametracker;
 
 import java.util.Objects;
@@ -6,22 +5,21 @@ import org.joda.time.DateTime;
 
 /**
  *
- * Basic data about a single play session. Includes the game
- * that was played, the date of the session and how long (in hours) the session
- * went on for.
- * 
+ * Basic data about a single play session. Includes the game that was played,
+ * the date of the session and how long (in hours) the session went on for.
+ *
  * @author tjkendon
  */
 public class PlaySession {
-    
-    private final Game game; 
+
+    private final Game game;
     private final DateTime sessionDate; // date the session was played on
     private final double playTime; // the length of time (in hours)
 
     /**
-     * 
-     * Creates a new PlaySession Instance 
-     * 
+     *
+     * Creates a new PlaySession Instance
+     *
      * @param game the game that was played
      * @param sessionDate the date the games was played
      * @param playTime how long (in hours) the game was played for
@@ -30,6 +28,13 @@ public class PlaySession {
         this.game = game;
         this.sessionDate = sessionDate;
         this.playTime = playTime;
+    }
+
+    PlaySession(PlaySession original) {
+        this.game = original.game;
+        this.sessionDate = original.sessionDate;
+        this.playTime = original.playTime;
+
     }
 
     public Game getGame() {
@@ -49,7 +54,7 @@ public class PlaySession {
         int hash = 5;
         hash = 29 * hash + Objects.hashCode(this.game);
         hash = 29 * hash + Objects.hashCode(this.sessionDate);
-        hash = 29 * hash + (int) (Double.doubleToLongBits(this.playTime) 
+        hash = 29 * hash + (int) (Double.doubleToLongBits(this.playTime)
                 ^ (Double.doubleToLongBits(this.playTime) >>> 32));
         return hash;
     }
@@ -66,7 +71,7 @@ public class PlaySession {
             return false;
         }
         final PlaySession other = (PlaySession) obj;
-        if (Double.doubleToLongBits(this.playTime) 
+        if (Double.doubleToLongBits(this.playTime)
                 != Double.doubleToLongBits(other.playTime)) {
             return false;
         }
@@ -78,10 +83,12 @@ public class PlaySession {
         }
         return true;
     }
-    
-    
-    
-    
-    
-    
+
+    @Override
+    public String toString() {
+        return sessionDate.toString("dd/MM/YYYY")
+                + ", " + game.getName()
+                + ", " + playTime;
+    }
+
 }
