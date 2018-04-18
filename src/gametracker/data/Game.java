74fa10b6c44cd3a015a61a2898b77be8b7acc470
Tyson Kendon <tj@kendon.ca>
2018-04-17 22:44:47 -0600

    Moved parsing methods into data classes to allow parsing from multiple data sources. Also moved the format for date time into playsession to similarly allow common use.

--- src/gametracker/data/Game.java
+++ src/gametracker/data/Game.java
@@ -1,17 +1,16 @@
-
 package gametracker.data;
 
 import java.util.Objects;
 
 /**
  *
- * Basic data about a game, including the name, the platform, and the year it 
+ * Basic data about a game, including the name, the platform, and the year it
  * was released.
- * 
+ *
  * @author tjkendon
  */
 public class Game {
-    
+
     /**
      * Enum covering platforms for games which I own :-)
      */
@@ -25,32 +24,31 @@ public class Game {
         Super_Nintendo,
         DS_3DS,
         DS,
-        GameBoy_Advance,        
+        GameBoy_Advance,
         Playstation_3,
         Playstation_2,
         Playstation,
         Unknown
-        
+
     }
-    
+
     private final String name;
     private final Platform platform;
     private final int year;
 
     /**
-     * 
+     *
      * Creates a new game with the given name, platform and year of creation
-     * 
+     *
      * @param name
      * @param platform
-     * @param year 
+     * @param year
      */
     public Game(String name, Platform platform, int year) {
         this.name = name;
         this.platform = platform;
         this.year = year;
     }
-    
 
     public String getName() {
         return name;
@@ -63,7 +61,7 @@ public class Game {
     public int getYear() {
         return year;
     }
-    
+
     public String toString() {
         return this.getName() + " (" + this.getPlatform() + ") - " + year;
     }
@@ -100,11 +98,47 @@ public class Game {
         }
         return true;
     }
-    
-    
-    
-    
-    
-    
-    
+
+    /**
+     *
+     * Parses the platform from a given string. Will return the platform which
+     * matches the valueOf for the string, if possible or throws an
+     * IllegalArgumentException otherwise.
+     *
+     * @param platformStr
+     * @return
+     */
+    public static Platform parsePlatform(String platformStr) {
+
+        try {
+            return Game.Platform.valueOf(platformStr);
+        } catch (IllegalArgumentException e) {
+            throw new IllegalArgumentException(
+                    "Not able to parse platform from " + platformStr, e);
+        }
+
+    }
+
+    /**
+     *
+     * Parses the year from a given string. Will return the year provided the
+     * string describes a year (is a 4 digit int) and throws an
+     * IllegalArgumentException.
+     *
+     * @param yearStr
+     * @return
+     */
+    public static int parseYear(String yearStr) {
+        int year = 0;
+        if (yearStr.length() == 4) {
+            try {
+                return Integer.parseInt(yearStr);
+            } catch (NumberFormatException e) {
+                throw new IllegalArgumentException("Not able to parse year from " + yearStr, e);
+            }
+        } else {
+            throw new IllegalArgumentException("Not able to parse year from " + yearStr);
+        }
+    }
+
 }
