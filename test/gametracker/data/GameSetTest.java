64add08427cd9b67f91da94570215c0756f6083e
Tyson Kendon <tj@kendon.ca>
2018-04-24 16:48:02 -0600

    Added test for isEmpty, in GameSet

--- test/gametracker/data/GameSetTest.java
+++ test/gametracker/data/GameSetTest.java
@@ -22,22 +22,22 @@ import org.junit.rules.ExpectedException;
  * @author tjkendon
  */
 public class GameSetTest {
-    
+
     public GameSetTest() {
     }
-    
+
     @BeforeClass
     public static void setUpClass() {
     }
-    
+
     @AfterClass
     public static void tearDownClass() {
     }
-    
+
     @Before
     public void setUp() {
     }
-    
+
     @After
     public void tearDown() {
     }
@@ -61,7 +61,7 @@ public class GameSetTest {
         instance.addGame(tg2);
         Set<Game> result = instance.getGames();
         assertEquals(expResult, result);
-        
+
     }
 
     /**
@@ -69,55 +69,54 @@ public class GameSetTest {
      */
     @Test
     public void testAddGame() {
-        System.out.println("Testing adding to GameSet and retreiving " +
-                "with exact name");
+        System.out.println("Testing adding to GameSet and retreiving "
+                + "with exact name");
         Game g = new Game("TG1", Game.Platform.PC_Steam, 2000);
         GameSet instance = new GameSet();
         instance.addGame(g);
-        
+
         assertEquals(instance.getGame("TG1", Game.Platform.PC_Steam, 2000), g);
     }
 
     /**
      * Test of getGame method, of class GameSet.
      */
-    
     @Rule
     public ExpectedException getGameNoCorrectRule = ExpectedException.none();
-    
+
     @Test
     public void testGetGame_NoCorrect() {
         System.out.println("Testing getting from GameSet by name with no match");
         String gameName = "TG1";
         GameSet instance = new GameSet();
-        
+
         getGameNoCorrectRule.expect(IllegalArgumentException.class);
         getGameNoCorrectRule.expectMessage(startsWith("Not able to find game"));
-        
+
         instance.getGame(gameName);
     }
 
-        @Rule
+    @Rule
     public ExpectedException getGameMultiCorrectRule = ExpectedException.none();
-    
+
     @Test
     public void testGetGame_MulitCorrect() {
-        System.out.println("Testing getting from GameSet by name with " + 
-                "more than 1 match");
+        System.out.println("Testing getting from GameSet by name with "
+                + "more than 1 match");
         String gameName = "TG1";
-        
+
         GameSet instance = new GameSet();
-        
+
         instance.addGame(new Game(gameName, Game.Platform.PC_Steam, 2000));
         instance.addGame(new Game(gameName, Game.Platform.PC_Steam, 2001));
-        
+
         getGameMultiCorrectRule.expect(IllegalArgumentException.class);
         getGameMultiCorrectRule.expectMessage(
                 startsWith("Multiple games with name"));
-        
+
         instance.getGame(gameName);
     }
-    
+
     /**
      * Test of getGame method, of class GameSet.
      */
@@ -133,30 +132,28 @@ public class GameSetTest {
         Game result = instance.getGame(gameName, p, year);
         assertEquals(expResult, result);
     }
-    
-            @Rule
+
+    @Rule
     public ExpectedException getGameNotMatchRule = ExpectedException.none();
 
-    
-        /**
+    /**
      * Test of getGame method, of class GameSet.
      */
     @Test
     public void testGetGame_NotExists() {
-        System.out.println("Testing getting a game with all data matching not" + 
-                " matching");
+        System.out.println("Testing getting a game with all data matching not"
+                + " matching");
         String gameName = "TG1";
         Game.Platform p = Game.Platform.PC_Steam;
         int year = 2000;
         GameSet instance = new GameSet();
-        
+
         getGameNotMatchRule.expect(IllegalArgumentException.class);
         getGameNotMatchRule.expectMessage(
                 startsWith("Not able to find game"));
-        
-        
+
         Game result = instance.getGame(gameName, p, year);
-        
+
     }
 
     /**
@@ -171,20 +168,19 @@ public class GameSetTest {
         Game g3 = new Game("TG2", Game.Platform.PC_Steam, 2000);
         Game g4 = new Game("TG2", Game.Platform.PC_Steam, 2001);
         GameSet instance = new GameSet();
-        
-        
+
         instance.addGame(g1);
         instance.addGame(g2);
         instance.addGame(g3);
         instance.addGame(g4);
-        
+
         Set<Game> expResult = new HashSet<>();
         expResult.add(g1);
         expResult.add(g2);
-        
+
         Set<Game> result = instance.getGames(gameName);
         assertEquals(expResult, result);
-        
+
     }
 
     /**
@@ -192,27 +188,26 @@ public class GameSetTest {
      */
     @Test
     public void testGetGames_GamePlatform() {
-        System.out.println("Testing getting all games that match a given " +
-                "Platform");
+        System.out.println("Testing getting all games that match a given "
+                + "Platform");
         Game.Platform platform = Game.Platform.DS;
         Game g1 = new Game("TG1", platform, 2000);
         Game g2 = new Game("TG1", Game.Platform.PC_Steam, 2001);
         Game g3 = new Game("TG2", Game.Platform.PC_Steam, 2000);
         Game g4 = new Game("TG2", platform, 2001);
-        
+
         GameSet instance = new GameSet();
         instance.addGame(g1);
         instance.addGame(g2);
         instance.addGame(g3);
         instance.addGame(g4);
-        
-        
+
         Set<Game> expResult = new HashSet<>();
         expResult.add(g1);
         expResult.add(g4);
         Set<Game> result = instance.getGames(platform);
         assertEquals(expResult, result);
-        
+
     }
 
     /**
@@ -222,44 +217,59 @@ public class GameSetTest {
     public void testGetGames_year() {
         System.out.println("Testing getting all games that match a given year");
         int year = 2000;
-        
-                Game g1 = new Game("TG1", Game.Platform.PC_Steam, year);
+
+        Game g1 = new Game("TG1", Game.Platform.PC_Steam, year);
         Game g2 = new Game("TG1", Game.Platform.PC_Steam, 2001);
         Game g3 = new Game("TG2", Game.Platform.PC_Steam, year);
         Game g4 = new Game("TG2", Game.Platform.PC_Steam, 2001);
-        
+
         GameSet instance = new GameSet();
         instance.addGame(g1);
         instance.addGame(g2);
         instance.addGame(g3);
         instance.addGame(g4);
-        
+
         Set<Game> expResult = new HashSet<>();
         expResult.add(g1);
         expResult.add(g3);
-       
+
         Set<Game> result = instance.getGames(year);
         assertEquals(expResult, result);
-        
+
     }
 
     @Test
     public void testContains() {
         System.out.println("Testing if a GameSet contains a game");
-        
+
         Game g1 = new Game("TG1", Game.Platform.PC_Steam, 2000);
-        
+
         GameSet instance = new GameSet();
         assertFalse(instance.contains(g1));
-        
+
         instance.addGame(g1);
         assertTrue(instance.contains(g1));
-        
+
         instance.removeGame(g1);
         assertFalse(instance.contains(g1));
     }
-    
-    
+
+    @Test
+    public void testEmpty() {
+        System.out.println("Testing if a GameSet isEmpty");
+
+        Game g1 = new Game("TG1", Game.Platform.PC_Steam, 2000);
+
+        GameSet instance = new GameSet();
+        assertTrue(instance.isEmpty());
+
+        instance.addGame(g1);
+        assertFalse(instance.isEmpty());
+
+        instance.removeGame(g1);
+        assertTrue(instance.isEmpty());
+    }
+
     /**
      * Test of removeGame method, of class GameSet.
      */
@@ -276,12 +286,10 @@ public class GameSetTest {
         instance.addGame(g2);
         instance.addGame(g3);
         instance.addGame(g4);
-        
-        
+
         assertTrue(instance.removeGame(g1));
-        
-        
+
         assertFalse(instance.contains(g1));
     }
-    
+
 }
