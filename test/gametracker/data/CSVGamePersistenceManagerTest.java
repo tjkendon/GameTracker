30b2f92586128bbe406b7ee9c26fe732625a0c8c
Tyson Kendon <tj@kendon.ca>
2018-05-02 20:33:00 -0600

    Fixed text description of save test

--- test/gametracker/data/CSVGamePersistenceManagerTest.java
+++ test/gametracker/data/CSVGamePersistenceManagerTest.java
@@ -6,6 +6,7 @@
 package gametracker.data;
 
 import java.io.File;
+import static org.hamcrest.CoreMatchers.startsWith;
 import org.joda.time.DateTime;
 import org.junit.After;
 import org.junit.AfterClass;
@@ -13,6 +14,8 @@ import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
 import static org.junit.Assert.*;
+import org.junit.Rule;
+import org.junit.rules.ExpectedException;
 
 /**
  *
@@ -64,5 +67,42 @@ public class CSVGamePersistenceManagerTest {
         
         assertEquals(games, result);
     }
+    
+            @Rule
+    public ExpectedException noGameDataFileSaveRule = ExpectedException.none();
+
+    @Test
+    public void testSavePlayDataNoFile() {
+        System.out.println(
+                "Testing saving game data with no file");
+
+        noGameDataFileSaveRule.expect(IllegalStateException.class);
+        noGameDataFileSaveRule.expectMessage(
+                startsWith("Datafile not set"));
+
+        // set up games
+        Game testgame1 = new Game("Test1", Game.Platform.PC_Steam, 2000);
+        Game testgame2 = new Game("Test1", Game.Platform.PC_Steam, 2001);
+        GameSet games = new GameSet();
+        games.addGame(testgame1);
+        games.addGame(testgame2);
+
+        DateTime testDate1 = DateTime.now();
+
+        PlaySession session1 = new PlaySession(testgame1, testDate1, 1);
+        PlaySession session2 = new PlaySession(testgame2, testDate1, 0.75);
+
+        PlayData original = new PlayData();
+
+        original.addPlaySession(session1);
+        original.addPlaySession(session2);
+        
+        CSVSessionPersistenceManager instance
+                = new CSVSessionPersistenceManager();
+        instance.setGameSet(games);
+        instance.savePlayData(original);
+
+    }
+    
     
 }
