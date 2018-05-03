74f036f1f46263192b85ceef5c195927aeb7c056
Tyson Kendon <tj@kendon.ca>
2018-05-02 21:10:00 -0600

    Added test for loading with no file set

--- test/gametracker/data/CSVGamePersistenceManagerTest.java
+++ test/gametracker/data/CSVGamePersistenceManagerTest.java
@@ -22,22 +22,22 @@ import org.junit.rules.ExpectedException;
  * @author tjkendon
  */
 public class CSVGamePersistenceManagerTest {
-    
+
     public CSVGamePersistenceManagerTest() {
     }
-    
+
     @BeforeClass
     public static void setUpClass() {
     }
-    
+
     @AfterClass
     public static void tearDownClass() {
     }
-    
+
     @Before
     public void setUp() {
     }
-    
+
     @After
     public void tearDown() {
     }
@@ -52,33 +52,33 @@ public class CSVGamePersistenceManagerTest {
     public void testSaveAndLoadGameData() {
         System.out.println(
                 "Testing saving and loading game data (in that order)");
-        
+
         // set up games
         Game testgame1 = new Game("Test1", Game.Platform.PC_Steam, 2000);
         Game testgame2 = new Game("Test2", Game.Platform.PC_Steam, 2001);
         GameSet games = new GameSet();
         games.addGame(testgame1);
         games.addGame(testgame2);
-        
-        CSVGamePersistenceManager instance = 
-                new CSVGamePersistenceManager(new File("data/test/game.data"));
+
+        CSVGamePersistenceManager instance
+                = new CSVGamePersistenceManager(new File("data/test/game.data"));
         instance.saveGameSet(games);
         GameSet result = instance.load();
-        
+
         assertEquals(games, result);
     }
-    
-            @Rule
+
+    @Rule
     public ExpectedException noGameDataFileSaveRule = ExpectedException.none();
 
     @Test
-    public void testSavePlayDataNoFile() {
+    public void testSaveGameDataNoFile() {
         System.out.println(
                 "Testing saving game data with no file");
 
         noGameDataFileSaveRule.expect(IllegalStateException.class);
         noGameDataFileSaveRule.expectMessage(
-                startsWith("Datafile not set"));
+                startsWith("No File Set to Save To"));
 
         // set up games
         Game testgame1 = new Game("Test1", Game.Platform.PC_Steam, 2000);
@@ -87,22 +87,29 @@ public class CSVGamePersistenceManagerTest {
         games.addGame(testgame1);
         games.addGame(testgame2);
 
-        DateTime testDate1 = DateTime.now();
+        CSVGamePersistenceManager instance = new CSVGamePersistenceManager();
 
-        PlaySession session1 = new PlaySession(testgame1, testDate1, 1);
-        PlaySession session2 = new PlaySession(testgame2, testDate1, 0.75);
-
-        PlayData original = new PlayData();
-
-        original.addPlaySession(session1);
-        original.addPlaySession(session2);
-        
-        CSVSessionPersistenceManager instance
-                = new CSVSessionPersistenceManager();
-        instance.setGameSet(games);
-        instance.savePlayData(original);
+        instance.saveGameSet(games);
 
     }
-    
-    
+
+    @Rule
+    public ExpectedException noGameDataFileLoadRule = ExpectedException.none();
+
+    @Test
+    public void testLoadGameDataNoFile() {
+        System.out.println(
+                "Testing loading game data with no file");
+
+        noGameDataFileLoadRule.expect(IllegalStateException.class);
+        noGameDataFileLoadRule.expectMessage(
+                startsWith("No File Set to Load From"));
+
+
+        CSVGamePersistenceManager instance = new CSVGamePersistenceManager();
+
+        GameSet load = instance.load();
+
+    }
+
 }
