be4643f7e01dd52d5d0dee09c745beb2496f6a28
Tyson Kendon <tj@kendon.ca>
2018-05-04 02:01:36 -0600

    Added alternate format for session lines
    To allow the system to read its own play session data or to read play session date from the google doc, extended the parser to identify either 5 (full) or 3 (short) formats and parse them if possible.

--- src/gametracker/data/CSVSessionPersistenceManager.java
+++ src/gametracker/data/CSVSessionPersistenceManager.java
@@ -21,9 +21,9 @@ public class CSVSessionPersistenceManager implements SessionPersistenceManager {
     private GameSet gameSet;
 
     public CSVSessionPersistenceManager() {
-        
+
     }
-    
+
     public CSVSessionPersistenceManager(File datafile, GameSet gameSet) {
         this.datafile = datafile;
         this.gameSet = gameSet;
@@ -44,25 +44,22 @@ public class CSVSessionPersistenceManager implements SessionPersistenceManager {
     public void setGameSet(GameSet gameSet) {
         this.gameSet = gameSet;
     }
-    
-    
-    
 
     @Override
     public PlayData load() {
-        
+
         PlayData returnData = new PlayData();
-        
+
         if (datafile == null) {
             throw new IllegalStateException(
                     "Datafile not set to load session data");
         }
-        
+
         if (gameSet == null) {
             throw new IllegalStateException(
                     "GameSet not set to load session data");
         }
-        
+
         try {
             Scanner scanner = new Scanner(datafile);
 
@@ -71,22 +68,17 @@ public class CSVSessionPersistenceManager implements SessionPersistenceManager {
                 String playString = scanner.nextLine();
                 String[] playStrings = playString.split(",");
                 try {
-                    String dateString = playStrings[0].trim();
-                    DateTime date = PlaySession.parseDateTime(dateString);
-
-                    String gameString = playStrings[1].trim();
-                    Game.Platform platform = 
-                            Game.parsePlatform(playStrings[2].trim());
-                    int year = Game.parseYear(playStrings[3].trim());
-                    
-                    
-                    Game game = gameSet.getGame(gameString, platform, year);
-
-                    String timeString = playStrings[4].trim();
-                    Double time = PlaySession.parsePlayTime(timeString);
-
-                    returnData.addPlaySession(
-                            new PlaySession(game, date, time));
+                    PlaySession s = null;
+                   if (playStrings.length == 3) {
+                       s = parseShort(playStrings);
+                   } else if (playStrings.length == 5) {
+                       s = parseFull(playStrings);
+                   } else {
+                       System.err.println("Line " + lineNumber
+                            + ": not added - Format not recognized");
+                       continue;
+                   }
+                   returnData.addPlaySession(s);
                 } catch (Exception e) {
                     System.err.println("Line " + lineNumber
                             + ": not added - " + e.getMessage());
@@ -104,15 +96,66 @@ public class CSVSessionPersistenceManager implements SessionPersistenceManager {
         return returnData;
     }
 
+    /**
+     * Attempts to parse a play session from 3 terms in an array of strings.
+     * (Date, Game, Time).
+     * 
+     * Will fail if the game name is not unique.
+     * 
+     * 
+     * @param playStrings array of data to attempt to read
+     * @return play session from provided data
+     */
+    private PlaySession parseShort(String[] playStrings) {
+        String dateString = playStrings[0].trim();
+        DateTime date = PlaySession.parseDateTime(dateString);
+
+        String gameString = playStrings[1].trim();
+
+        Game game = gameSet.getGame(gameString);
+
+        String timeString = playStrings[2].trim();
+        Double time = PlaySession.parsePlayTime(timeString);
+
+        return new PlaySession(game, date, time);
+    }
+
+    
+        /**
+     * Attempts to parse a play session from 5 terms in an array of strings.
+     * (Date, Game Name, Platform, Year, Time).
+     * 
+     * Will fail if the game is not in the game set
+     * 
+     * 
+     * @param playStrings array of data to attempt to read
+     * @return play session from provided data
+     */
+    private PlaySession parseFull(String[] playStrings) {
+        String dateString = playStrings[0].trim();
+        DateTime date = PlaySession.parseDateTime(dateString);
+
+        String gameString = playStrings[1].trim();
+        Game.Platform platform
+                = Game.parsePlatform(playStrings[2].trim());
+        int year = Game.parseYear(playStrings[3].trim());
+
+        Game game = gameSet.getGame(gameString, platform, year);
+
+        String timeString = playStrings[4].trim();
+        Double time = PlaySession.parsePlayTime(timeString);
+
+        return new PlaySession(game, date, time);
+    }
+
     @Override
     public void savePlayData(PlayData sessions) {
-        
-        
+
         if (datafile == null) {
             throw new IllegalStateException(
                     "Datafile not set to save session data");
         }
-        
+
         try (PrintWriter writer = new PrintWriter(datafile)) {
             for (PlaySession s : sessions.getPlaySessions()) {
                 writer.printf("%s, %s, %s, %s, %s%n",
@@ -128,6 +171,5 @@ public class CSVSessionPersistenceManager implements SessionPersistenceManager {
         }
 
     }
-    
 
 }
