ef032619cd0891bb2d8d60ef8e5c9fe43e6808b5
Tyson Kendon <tj@kendon.ca>
2018-05-29 13:29:17 -0600

    Added test for filtering dates before
    Added stubs to the Date Filter class as needed.

--- src/gametracker/data/DateFilter.java
+++ src/gametracker/data/DateFilter.java
@@ -5,6 +5,8 @@
  */
 package gametracker.data;
 
+import org.joda.time.DateTime;
+
 /**
  *
  * @author tjkendon
@@ -13,6 +15,10 @@ public class DateFilter implements Filter{
 
     @Override
     public PlayData filter(PlayData source) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    void addWindow(DateTime beginingTime, DateTime endTime) {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
     
