a4cffe503c190c87838f8394e9b9acafc9dfe7c7
Tyson Kendon <tj@kendon.ca>
2018-06-05 11:04:17 -0600

    Added Aggregator interface and Aggregate class.
    Aggregator will structure how new aggregates are calculated and added to the data.
    
    The PlayAggregate class contains a map of games & data, for each game there can be zero or more aggregate fields. Each field includes a type and a value. At this point all values must be doubles (which works with the current data we might aggregate).

---
+++ src/gametracker/data/PlayAggregate.java
@@ -0,0 +1,108 @@
+package gametracker.data;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * 
+ * Class to hold data about a game aggregated from PlayData. Contains a mapping
+ * of games to data, and each piece of data is constructed of a Type (listed
+ * below and a value - a double).
+ *
+ * @author tjkendon
+ */
+class PlayAggregate {
+
+    private final Map<Game, Map<AggregateType, Double>> aggregate;
+
+    /**
+     * Labels the different types of aggregate the system can contain.
+     */
+    public enum AggregateType {
+        /** 
+         * Value is the total time a game was played in the PlayData.
+         */
+        TOTAL_TIME,
+        /**
+         * Value is the total number of times the game was played in the 
+         * PlayData.
+         */
+        TOTAL_COUNT,
+        /**
+         * Value is the mean time the game was played in in the PlayData.
+         */
+        AVERAGE_TIME,
+        /**
+         * Value is the median time the game was played in the PlayData.
+         */
+        MEDIAN_TIME
+    }
+
+    /**
+     * Creates a new aggregate with no date included.
+     */
+    public PlayAggregate() {
+        aggregate = new HashMap<>();
+    }
+
+    /**
+     * 
+     * Adds or updates an aggregate value for the given game.
+     * 
+     * @param game the game to update
+     * @param type the type of data to update
+     * @param value the new or updated value
+     */
+    public void putAggregate(Game game, AggregateType type, double value) {
+
+        if (!aggregate.containsKey(game)) {
+            aggregate.put(game, new HashMap<>());
+        }
+        
+        aggregate.get(game).put(type, value);
+    }
+    
+    /**
+     * 
+     * Returns a copy of the full map of aggregate data.
+     * 
+     * @return a full map of game, data pairs 
+     */
+    public Map<Game, Map<AggregateType, Double>>  getAggregates() {
+        return new HashMap<> (aggregate);
+    }
+    
+    /**
+     * 
+     * Returns a copy of the data fields for a particular game
+     * 
+     * @param game the game to get the data for
+     * @return a map of type, value pairs
+     */
+    public Map<AggregateType, Double> getAggregatesForGame(Game game) {
+        return new HashMap<>(aggregate.get(game));
+    }
+
+    /**
+     * 
+     * Resets the aggregate to its initial state, with no data included.
+     * 
+     */
+    public void clear() {
+        aggregate.clear();
+    }
+    
+    /** 
+     * 
+     * Resets the data for a particular game. So that there will be no
+     * data included for the particular game.
+     * 
+     * @param game the game to reset the data for.
+     */
+    public void clearGame(Game game) {
+        if (aggregate.containsKey(game)) {
+            aggregate.get(game).clear();
+        }
+    }
+    
+}
