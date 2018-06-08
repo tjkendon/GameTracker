44666c314c812383a6358999be0f892a59a5cc52
Tyson Kendon <tj@kendon.ca>
2018-06-07 23:22:07 -0600

    Added average aggregator and tests

---
+++ src/gametracker/data/AverageTimeAggregator.java
@@ -0,0 +1,56 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package gametracker.data;
+
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ *
+ * @author tjkendon
+ */
+public class AverageTimeAggregator implements Aggregator {
+    
+    PlayData sourceData;
+    
+    public final PlayAggregate.AggregateType type = 
+            PlayAggregate.AggregateType.AVERAGE_TIME;
+    
+    AverageTimeAggregator(PlayData source) {
+        this.sourceData = source;
+    }
+
+    @Override
+    public PlayAggregate aggregate() {
+        
+        Set<Game> games = new HashSet<>();
+        
+        PlayAggregate returnData = new PlayAggregate();
+        
+        for (PlaySession session : sourceData.getPlaySessions()) {
+            games.add(session.getGame());
+        }
+        
+        for (Game game : games) {
+            double totalTime = 0;
+            double count = 0;
+            for (PlaySession session: sourceData.getPlaySessions()) {
+                if (session.getGame().equals(game)) {
+                    totalTime += session.getPlayTime();
+                    count += 1;
+                }
+            }
+            returnData.putAggregate(game, type, totalTime / count);
+        }
+        
+        return returnData;
+        
+                
+        
+        
+    }
+    
+}
