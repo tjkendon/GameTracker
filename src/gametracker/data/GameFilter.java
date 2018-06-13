aa014d08bcfc0d1961f252dcde3953e6f892f753
Tyson Kendon <tj@kendon.ca>
2018-06-13 13:23:26 -0600

    Fixed listing of games in GameFileter toString()

--- src/gametracker/data/GameFilter.java
+++ src/gametracker/data/GameFilter.java
@@ -8,6 +8,8 @@ package gametracker.data;
 import java.util.List;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.StringJoiner;
+import java.util.stream.Collectors;
 
 /**
  *
@@ -15,7 +17,9 @@ import java.util.Arrays;
  */
 public class GameFilter implements Filter {
 
-    public List<Game> filterGames = new ArrayList<>();;
+    public List<Game> filterGames = new ArrayList<>();
+
+    ;
 
     /**
      * 
@@ -25,17 +29,16 @@ public class GameFilter implements Filter {
      * 
      */
     GameFilter() {
-        
+
     }
-    
-    
+
     /**
-     * 
+     *
      * Creates a new filter with the games listed in the arguments in the filter
      *
      * When used, it will a data set with any session using any game in the
      * filter included.
-     * 
+     *
      * @param games the game (or games) to add
      */
     public GameFilter(Game... games) {
@@ -43,122 +46,121 @@ public class GameFilter implements Filter {
     }
 
     /**
-     * 
+     *
      * Creates a new filter with the games in the list in the filter
      *
      * When used, it will a data set with any session using any game in the
      * filter included.
-     * 
+     *
      * @param games the games to add
      */
     GameFilter(List<Game> games) {
         addAllGames(games);
     }
-    
+
     /**
-     * 
-     * Adds all games to the filter. Sessions with any of the games
-     * will be included when the filter is called.
-     * 
-     * @param games the games  to add to this filter
+     *
+     * Adds all games to the filter. Sessions with any of the games will be
+     * included when the filter is called.
+     *
+     * @param games the games to add to this filter
      */
     public final void addAllGames(Game... games) {
         addAllGames(Arrays.asList(games));
-        
+
     }
-    
+
     /**
-     * 
+     *
      * Adds all games in the list to the filter. Sessions with any of the games
      * will be included when the filter is called.
-     * 
+     *
      * @param games the list of games add to this filter
      */
     public final void addAllGames(List<Game> games) {
         filterGames.addAll(games);
     }
-    
+
     /**
-     * 
-     * Removes games from the filter. Session with any of the games
-     * will not be included when the filter is called.
-     * 
+     *
+     * Removes games from the filter. Session with any of the games will not be
+     * included when the filter is called.
+     *
      * @param games the games to remove from the filter
-     * 
+     *
      * @return true if the games could be removed
      */
     public final boolean removeAllGames(Game... games) {
         return removeAllGames(Arrays.asList(games));
     }
-    
+
     /**
-     * 
+     *
      * Removes games in the list from the filter. Session with any of the games
      * will not be included when the filter is called.
-     * 
+     *
      * @param games the games to remove from the filter
-     * 
+     *
      * @return true if the games could be removed
      */
     public final boolean removeAllGames(List<Game> games) {
-       return filterGames.removeAll(games);
+        return filterGames.removeAll(games);
     }
-    
+
     /**
-     * 
-     * Resets the filter by removing all games from it. After calling
-     * the filter will be empty and if filter is called it will return
-     * an empty PlayData.
-     * 
+     *
+     * Resets the filter by removing all games from it. After calling the filter
+     * will be empty and if filter is called it will return an empty PlayData.
+     *
      */
     public final void clear() {
         filterGames.clear();
     }
-    
+
     /**
-     * 
+     *
      * Returns a data set which is a copy subset of the source, including only
      * play sessions with games that have been added to the filter. A filter
      * with no games will return an empty data set.
-     * 
+     *
      * @param source the original play data
-     * 
+     *
      * @return a new play data object with deep copies of the play sessions
      */
     @Override
     public PlayData filter(PlayData source) {
-        
+
         PlayData returnData = new PlayData();
-        
-        source.getPlaySessions().stream().filter((session) 
+
+        source.getPlaySessions().stream().filter((session)
                 -> (filterGames.contains(
-                        session.getGame()))).forEachOrdered((session) 
-                        -> {
+                        session.getGame()))).forEachOrdered((session)
+                -> {
             returnData.addPlaySession(session);
         });
-        
+
         return returnData;
-        
+
     }
-    
+
     /**
-     * 
+     *
      * Reports a list of the games in the filter.
-     * 
-     * @return 
-     * 
+     *
+     * @return
+     *
      */
     @Override
     public String toString() {
-       StringBuilder br = new StringBuilder("Filter: [");
-       for (Game g : filterGames) {
-           br.append(g.getName());
-           br.append(", ");
-       }
-       br.subSequence(0, br.lastIndexOf(","));
-       br.append("]");
-       return br.toString();
-       
+        StringBuilder br = new StringBuilder("Filter: [");
+        // fun with java joiner example from API
+        String commaSeparatedGames = filterGames.stream()
+                .map(i -> i.toString())
+                .collect(Collectors.joining(", "));
+
+        br.append(commaSeparatedGames.toString()).append("]");
+        return br.toString();
+
     }
-    
+
 }
