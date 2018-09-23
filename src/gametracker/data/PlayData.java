546c5f2a19d2b6f57c116716204176b3d31e2edc
Tyson Kendon <tj@kendon.ca>
2018-09-22 22:20:14 -0600

    Updated containsMatchingContent to work with LocalDate.
    Not sure why this function exists. Could not see where it was created in the commit history (That's a searching failure not an existance failure). testSaveAndLoadPlayData now passes.

--- src/gametracker/data/PlayData.java
+++ src/gametracker/data/PlayData.java
@@ -1,4 +1,3 @@
-
 package gametracker.data;
 
 import java.util.ArrayList;
@@ -6,18 +5,16 @@ import java.util.List;
 import java.util.Objects;
 import org.joda.time.DateTimeComparator;
 
-
 /**
  *
  * Holds a list of PlaySessions.
- * 
+ *
  * @author tjkendon
  */
 public class PlayData {
-    
+
     private final List<PlaySession> sessions;
     private boolean changed;
-
 
     /**
      * Creates a new PlayData with an empty list of sessions.
@@ -27,45 +24,44 @@ public class PlayData {
         changed = false;
 
     }
-    
+
     /**
      * Creates a new PlayData with a list of sessions copied from the original.
-     * 
+     *
      * Records that the data has been changed in the changed flag.
-     * 
-     * @param sourceData 
+     *
+     * @param sourceData
      */
     public PlayData(PlayData sourceData) {
         this.sessions = new ArrayList<>(sourceData.getPlaySessions());
         changed = true;
     }
-    
+
     /**
-     * 
+     *
      * Adds a new session to the list.
-     * 
-     * @param session 
+     *
+     * @param session
      */
     public void addPlaySession(PlaySession session) {
         this.sessions.add(session);
         changed = true;
 
     }
-    
-    
+
     /**
-     * 
+     *
      * Returns a copy of the list of sessions in this data.
-     * 
-     * @return 
+     *
+     * @return
      */
     public List<PlaySession> getPlaySessions() {
         List<PlaySession> returnList = new ArrayList<>();
-        
+
         for (PlaySession s : sessions) {
             returnList.add(s);
         }
-        
+
         return returnList;
     }
 
@@ -99,41 +95,33 @@ public class PlayData {
         return Objects.equals(this.sessions, other.sessions);
     }
 
-    
-    
-
     public boolean hasChanged() {
         return changed;
     }
-    
+
     public void resetChanged() {
         changed = false;
     }
-    
+
     public static boolean containsMatchingContent(PlayData a, PlayData b) {
-        DateTimeComparator dateComp = DateTimeComparator.getDateOnlyInstance();
+
         if (a.getPlaySessions().size() == b.getPlaySessions().size()) {
             for (int i = 0; i < a.getPlaySessions().size(); i++) {
                 if ((a.getPlaySessions().get(i).getGame().equals(
-                        (b.getPlaySessions().get(i).getGame()))) &&
-                        (dateComp.compare(
-                                a.getPlaySessions().get(i).getSessionDate(),
-                                b.getPlaySessions().get(i).getSessionDate()) 
-                                    == 0) &&
-                        (a.getPlaySessions().get(i).getPlayTime() == 
-                            b.getPlaySessions().get(i).getPlayTime())
-                        ){
-                    
+                        (b.getPlaySessions().get(i).getGame())))
+                        && a.getPlaySessions().get(i).getSessionDate().equals(
+                                b.getPlaySessions().get(i).getSessionDate())
+                        && (a.getPlaySessions().get(i).getPlayTime()
+                        == b.getPlaySessions().get(i).getPlayTime())) {
+
                 } else {
                     return false;
                 }
-                                
+
             }
             return true;
         }
         return false;
-    } 
-    
-    
-    
+    }
+
 }
