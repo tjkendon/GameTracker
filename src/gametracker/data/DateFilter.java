36f2db22863f7090dfaf46119bc8e7e0d27397f4
Tyson Kendon <tj@kendon.ca>
2018-10-06 23:37:53 -0600

    Cleaned up documentation and tweeked style

--- src/gametracker/data/DateFilter.java
+++ src/gametracker/data/DateFilter.java
@@ -1,6 +1,7 @@
 package gametracker.data;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 import java.util.stream.Collectors;
 import org.joda.time.LocalDate;
@@ -9,13 +10,24 @@ import org.joda.time.format.DateTimeFormatter;
 
 /**
  *
- * Data Filter
+ * Filters a {@link PlayData} to view only specific dates. Dates are organized
+ * in {@link Window}s, any play session with a date between the start and end
+ * dates of a window in the filter is shown, and any play session with a date
+ * not included is not shown.
  *
- * @author tjkendon
  */
 public class DateFilter implements Filter {
 
-    List<Window> windows;
+    /**
+     * Accepted format for the start and end dates of a {@link Window}
+     */
+    public static final DateTimeFormatter DATE_FORMAT
+            = DateTimeFormat.forPattern("yyyy/MM/dd");
+
+    /**
+     * The list of windows for this filter.
+     */
+    private final List<Window> windows;
 
     /**
      *
@@ -29,21 +41,21 @@ public class DateFilter implements Filter {
 
     /**
      *
-     * Filters the PlayData source and returns all sessions with a date that is
-     * inside one of the windows in the filter.
+     * Filters the {@link PlayData} and returns all sessions with a date inside
+     * one of the windows in the filter.
      *
-     * @param source
-     * @return
+     * @param source the original play sessions to be filtered
+     * @return all play sessions that have a date within a window in this filter
      */
     @Override
     public PlayData filter(PlayData source) {
         PlayData returnData = new PlayData();
 
         source.getPlaySessions().forEach((s) -> {
-            windows.stream().filter((w) -> 
-                    (w.isIn(s.getSessionDate()) && 
-                            (!returnData.contains(s)))).forEachOrdered((_item) 
-                                    -> {
+            windows.stream().filter((w)
+                    -> (w.isIn(s.getSessionDate())
+                    && (!returnData.contains(s)))).forEachOrdered((_item)
+                    -> {
                 returnData.addPlaySession(s);
             });
         });
@@ -57,39 +69,38 @@ public class DateFilter implements Filter {
      * date (inclusive) and before the end date (exclusive) will be passed
      * through the filter
      *
-     * @param beginingTime
-     * @param endTime
+     * @param startDate the start date for the new window
+     * @param endDate the end date for the new window
      */
-    public void addWindow(LocalDate beginingTime, LocalDate endTime) {
+    public void addWindow(LocalDate startDate, LocalDate endDate) {
 
         Window w = new Window();
 
-        w.setStart(beginingTime);
-        w.setEnd(endTime);
+        w.setStart(startDate);
+        w.setEnd(endDate);
         windows.add(w);
 
     }
 
     /**
      *
-     * Returns the list of all windows in this filter.
+     * Returns the list of all {@link windows} in this filter.
      *
-     * @return
+     * @return a list of of the windows in this filter
      */
     public List<Window> getWindows() {
-        return windows;
+        return Collections.unmodifiableList(windows);
     }
 
     /**
      *
+     * Removes a window from the filter.
      *
-     * Removes a given window from the filter.
-     *
-     * @param w
-     * @return
+     * @param window the window to be removed
+     * @return true if the window was removed
      */
-    public boolean removeWindow(Window w) {
-        return windows.remove(w);
+    public boolean removeWindow(Window window) {
+        return windows.remove(window);
     }
 
     /**
@@ -103,6 +114,13 @@ public class DateFilter implements Filter {
         windows.clear();
     }
 
+    /**
+     *
+     * Returns a representation of the Filter as a series of windows, separated
+     * by commas.
+     *
+     * @return a string listing all windows in the filter
+     */
     @Override
     public String toString() {
         StringBuilder br = new StringBuilder("Date Filter: [");
@@ -115,20 +133,24 @@ public class DateFilter implements Filter {
         return br.toString();
     }
 
+    /**
+     *
+     * Returns true if the filter has no windows.
+     *
+     * @return true if there are no windows in the filter.
+     */
     @Override
     public boolean isEmpty() {
         return windows.isEmpty();
     }
-    
-    
 
     /**
      *
-     * Class that holds the beginning and end times of a period.
+     * Holds the beginning and end times of a period.
      *
      * Starts with the end points set to null and will return true for all dates
-     * tested with isIn. Setting the Start Date, it will return true if the date
-     * is after the start date (inclusive). Setting the End Date, it will return
+     * tested. If the start date is set, it will return true if the date is
+     * after the start date (inclusive). If the end date is set, it will return
      * true if the date is before the end date (exclusive).
      *
      * If both are set it will return if the date is between the start date
@@ -137,33 +159,90 @@ public class DateFilter implements Filter {
      */
     public class Window {
 
-        public DateTimeFormatter DATE_FORMAT
-            = DateTimeFormat.forPattern("yyyy/MM/dd");
-        
-        LocalDate start;
-        LocalDate end;
+        /**
+         * Start date of the window, any session must have a date matching this 
+         * or later to be accepted by the filter.
+         */
+        private LocalDate start;
+        /**
+         * End date of the window, any session must have a date before this 
+         * to be accepted by the filter.
+         */
+        private LocalDate end;
 
+        /**
+         * 
+         * Creates a new window with no start or end date. This window will
+         * include all dates.
+         * 
+         */
         public Window() {
             start = null;
             end = null;
         }
 
+        /**
+         * 
+         * Returns the start date of the window. Sessions must have a date 
+         * on or after this to be accepted by the window.
+         * 
+         * @return the start date of the window
+         */
         public LocalDate getStart() {
             return start;
         }
 
+        /**
+         * 
+         * Sets the start date of the window. Sessions must have a date 
+         * on or after this to be accepted by the window. 
+         * A date of null will accept all dates.
+         * 
+         * @param start the date which all sessions must be on or after to be 
+         * accepted. null to accept all dates.
+         */
         public void setStart(LocalDate start) {
             this.start = start;
         }
 
+        /**
+         * Returns the end date of the window. Sessions must have a date before
+         * this to be accepted by the window.
+         * 
+         * @return the end date of the window
+         */
         public LocalDate getEnd() {
             return end;
         }
+
+        /**
+         * 
+         * Sets the end date of the window. Sessions must have a date 
+         * before this to be accepted by the window. 
+         * A date of null will accept all dates.
+         * 
+         * @param end the date which all sessions must be before to be 
+         * accepted. null to accept all dates.
+         */
 
         public void setEnd(LocalDate end) {
             this.end = end;
         }
 
+        /**
+         * 
+         * Checks if the given date falls within this window. Will return true
+         * so long as the date is on or before the window's start date and 
+         * before the window's end date.
+         * 
+         * If the start date is null it is considered equivalent to the 
+         * beginning of time. If the end date is null it is considered 
+         * equivalent to the end of time.
+         * 
+         * @param date the date to test against the window
+         * @return true if the date is within the window, false otherwise
+         * 
+         */
         public boolean isIn(LocalDate date) {
             if ((start == null) && (end == null)) {
                 return true;
@@ -172,10 +251,18 @@ public class DateFilter implements Filter {
             } else if ((start != null) && (end == null)) {
                 return date.isAfter(start) || date.isEqual(start);
             }
-            return (date.isAfter(start) || date.isEqual(start)) 
+            return (date.isAfter(start) || date.isEqual(start))
                     && date.isBefore(end);
         }
 
+        /**
+         * 
+         * Returns a string representation of the start and end dates
+         * of the window. If either is null, it is replaced with "Start of Time"
+         * or "End of Time" respectively, or "All of Time" if both are null.
+         * 
+         * @return a string representing the window via its start and end dates
+         */
         @Override
         public String toString() {
             if ((start == null) && (end == null)) {
@@ -185,11 +272,9 @@ public class DateFilter implements Filter {
             } else if ((start != null) && (end == null)) {
                 return "[" + DATE_FORMAT.print(start) + " - End of Time]";
             }
-            return "[" + DATE_FORMAT.print(start) + " - " 
+            return "[" + DATE_FORMAT.print(start) + " - "
                     + DATE_FORMAT.print(end) + "]";
         }
-        
-        
 
     }
 
