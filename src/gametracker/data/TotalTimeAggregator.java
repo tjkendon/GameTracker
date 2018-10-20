451cdae19b4847d2430b1db336c2b50f5b551e51
Tyson Kendon <tj@kendon.ca>
2018-10-19 23:09:53 -0600

    Updated documentation

--- src/gametracker/data/TotalTimeAggregator.java
+++ src/gametracker/data/TotalTimeAggregator.java
@@ -10,18 +10,38 @@ import java.util.Set;
 
 /**
  *
- * @author tjkendon
+ * 
+ * Produces an aggregate of the total time each game has been played for over
+ * all sessions in the {@link PlaySessionList}.
+ * 
  */
 public class TotalTimeAggregator extends Aggregator {
     
+    /**
+     * The flag to indicate the {@link PlayAggregate} contains the total time
+     * aggregate.
+     */
     public final PlayAggregate.AggregateType type = 
             PlayAggregate.AggregateType.TOTAL_TIME;
 
+    /**
+     * Creates a new aggregator with the given {@link PlaySessionList} as source
+     * data.
+     * @param sourceData the data this aggregator will aggregate.
+     */
     public TotalTimeAggregator(PlaySessionList sourceData) {
         super(sourceData);
     }
     
-
+    /**
+     * 
+     * Produces a {@link PlayAggregate} which includes the total length of time
+     * each game has been played for in all sessions in the source
+     * {@link PlaySessionList}.
+     * 
+     * @return an aggregate of the total time each game as been played in the
+     * source data.
+     */
     @Override
     public PlayAggregate aggregate() {
         
