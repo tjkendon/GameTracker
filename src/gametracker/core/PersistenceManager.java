e109c888eae69b7129580dc54bd1316ca767c353
Tyson Kendon <tj@kendon.ca>
2022-01-05 12:16:02 -0700

    Fixed javadoc tags

--- src/gametracker/core/PersistenceManager.java
+++ src/gametracker/core/PersistenceManager.java
@@ -4,37 +4,42 @@ import gametracker.data.GameSet;
 import gametracker.data.PlaySessionList;
 
 /**
- *
  * Provides the basic interface for managing persistence of game and play session data.
- *
+ * <p>
  * Assumes that there is a point in time where data will need to be gathered from the storage in the
  * implementation and equally that there is a point where data will be pushed to storage. Provides
  * access to the games and session information.
- *
  */
 public interface PersistenceManager {
 
     /**
-     *
-     * Brings data from storage into the manager.
-     *
+     * Brings data from storage into the <code>PersistanceManager</code>.
+     * <p>
      * Assumes that the data available internally will be up-to-date with the storage when
      * this method is called.
-     *
      */
     void load();
 
     /**
-     *
-     * Stores data in the manager.
-     *
+     * Stores data in the <code>PersistanceManager</code>.
+     * <p>
      * Assumes that after this method is called data in storage will be up-to-date with what
-     * is in the manager.
-     *
+     * is in the <code>PersistanceManager</code>.
      */
     void save();
 
-    public GameSet getGameSet();
+    /**
+     * Returns the current game set in the <code>PersistanceManager</code>.
+     *
+     * @return the current {@link GameSet}
+     */
+    GameSet getGameSet();
+
+    /**
+     * Returns the current play session list in the <code>PersistanceManager</code>.
+     *
+     * @return the current {@link PlaySessionList}
+     */
     public PlaySessionList getPlaySessionList();
 
 
